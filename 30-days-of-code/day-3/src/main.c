4e2cb2ed24c4f9733b3241943ef9a0cebb2417c1
Abner Castro <abnerfcastro@gmail.com>
2017-03-28 20:39:27 -0300

    fixing identation

--- 30-days-of-code/day-3/src/main.c
+++ 30-days-of-code/day-3/src/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int N;
-	scanf("%d", &N);
+    int N;
+    scanf("%d", &N);
 
-	if (N % 2 != 0)
-		printf("Weird");
-	else if (N % 2 == 0 && (N >= 2 && N <= 5))
-		printf("Not Weird");
-	else if (N % 2 == 0 && (N >= 6 && N <= 20))
-		printf("Weird");
+    if (N % 2 != 0)
+        printf("Weird");
+    else if (N % 2 == 0 && (N >= 2 && N <= 5))
+        printf("Not Weird");
+    else if (N % 2 == 0 && (N >= 6 && N <= 20))
+        printf("Weird");
     else if (N % 2 == 0 && N > 20)
-		printf("Not Weird");
+        printf("Not Weird");
 
-	return 0;
+    return 0;
 }
