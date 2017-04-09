f571bf6b928784379ae4ba413954603a221bb892
Abner Castro <abnerfcastro@gmail.com>
2017-04-09 18:45:44 -0300

    indentation issues

--- algorithms/implementation/00-grading/src/main.c
+++ algorithms/implementation/00-grading/src/main.c
@@ -10,34 +10,36 @@
 
 /*
  * Rounding rules:
- *		- if the value of grade is less than 38, no rounding occurs;
- *		- if the difference between the grade and the
- *		next multiple of 5 is less than 3, round grade up to the next
- *		multiple of 5;
+ *      - if the value of grade is less than 38, no rounding occurs;
+ *      - if the difference between the grade and the
+ *      next multiple of 5 is less than 3, round grade up to the next
+ *      multiple of 5;
  */
 int round_grade(const int grade)
 {
-	// if grade is less than 38, no rounding occurs
-	if (grade < 38)
-		return grade;
+    // if grade is less than 38, no rounding occurs
+    if (grade < 38)
+        return grade;
 	
-	// factor is how much to the next multiple of 5
-	int factor = 5 - (grade % 5);
-	int rounding_factor = (factor < 3) ? factor : 0;
-	return grade + rounding_factor;
+    // factor is how much to the next multiple of 5
+    int factor = 5 - (grade % 5);
+    int rounding_factor = (factor < 3) ? factor : 0;
+    return grade + rounding_factor;
 }
 
 int main()
 {
-	// how many grades to round
-	int n;
-	scanf("%d", &n);
+    // how many grades to round
+    int n;
+    scanf("%d", &n);
 
-	int grade;
+    int grade;
 
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &grade);
-		printf("%d\n", round_grade(grade));
-	}
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &grade);
+        printf("%d\n", round_grade(grade));
+    }
+
+    return 0;
 }
