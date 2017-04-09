55b935c46399046b0b6eb5b89cc13f14cf8872e5
Abner Castro <abnerfcastro@gmail.com>
2017-04-09 11:56:22 -0300

    fixing indentation

--- 30-days-of-code/day-4/src/main.cpp
+++ 30-days-of-code/day-4/src/main.cpp
@@ -2,51 +2,51 @@
 using namespace std;
 
 class Person {
-	public:
-		int age;
-		Person(int initialAge);
-		void amIOld();
-		void yearPasses();
+    public:
+        int age;
+        Person(int initialAge);
+        void amIOld();
+        void yearPasses();
 };
 
 Person::Person(int initialAge) {
-	if (initialAge < 0) {
-		cout << "Age is not valid, setting age to 0" << endl;
-		this->age = 0;
-	} else {
-		this->age = initialAge;
-	}	
+    if (initialAge < 0) {
+        cout << "Age is not valid, setting age to 0" << endl;
+        this->age = 0;
+    } else {
+        this->age = initialAge;
+    }	
 }
 
 void Person::amIOld() {
-	if (this->age < 13)
-		cout << "You are young." << endl;
-	else if (this->age >= 13 && this->age < 18)
-		cout << "You are a teenager." << endl;
-	else
-		cout << "You are old." << endl;
+    if (this->age < 13)
+        cout << "You are young." << endl;
+    else if (this->age >= 13 && this->age < 18)
+        cout << "You are a teenager." << endl;
+    else
+        cout << "You are old." << endl;
 }
 
 void Person::yearPasses() {
-	this->age++;
+    this->age++;
 }
 
 int main() {
-	int t;
-	int age;
-	cin >> t;
+    int t;
+    int age;
+    cin >> t;
 	
-	for (int i = 0; i < t; i++) {
-		cin >> age;
-		Person p(age);
-		p.amIOld();
-		for (int j = 0; j < 3; j++) {
-			p.yearPasses();
-		}
-		p.amIOld();
+    for (int i = 0; i < t; i++) {
+        cin >> age;
+        Person p(age);
+        p.amIOld();
+        for (int j = 0; j < 3; j++) {
+            p.yearPasses();
+        }
+        p.amIOld();
 
-		cout << '\n';
+        cout << '\n';
 	}
 
-	return 0;
+    return 0;
 }
