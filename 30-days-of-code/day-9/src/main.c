0a9c6d6f3ac8b4a8a6f7e6d34c3873631417f174
Abner Castro <abnerfcastro@gmail.com>
2017-04-23 01:55:50 -0300

    fixing indentation

--- 30-days-of-code/day-9/src/main.c
+++ 30-days-of-code/day-9/src/main.c
@@ -6,14 +6,13 @@
 int factorial(const int);
 
 int main() {
-	int n;
-	scanf("%d", &n);
-
-	printf("%d", factorial(n));
+    int n;
+    scanf("%d", &n);
+    printf("%d", factorial(n));
 }
 
 int factorial(const int n)
 {
-	if (n <= 1) return 1;
-	return n * factorial(n - 1);
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
 }
