7f8b28c54ca7472edda9684c11e0487cc2e59e50
Abner Castro <abnerfcastro@gmail.com>
2017-04-23 01:03:38 -0300

    fixing indentation

--- 30-days-of-code/day-7/src/main.c
+++ 30-days-of-code/day-7/src/main.c
@@ -3,20 +3,22 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
-	int size;
-	scanf("%d", &size);
+int main()
+{
+    int size;
+    scanf("%d", &size);
 
-	int array[size];
-	for (int i = 0; i < size; i++)
-		scanf("%d", &array[i]);
-	
-	for (int i = size - 1; i >= 0; i--)
-	{
-		printf("%d", array[i]);		
-		// needs this so it won't print space at the end
-		if (i > 0) printf(" ");
-	}
+    int array[size];
+    for (int i = 0; i < size; i++)
+        scanf("%d", &array[i]);
 
-	return 0;		
+    for (int i = size - 1; i >= 0; i--)
+    {
+        printf("%d", array[i]);
+        // needs this so it won't print space at the end
+        if (i > 0)
+            printf(" ");
+    }
+
+    return 0;
 }
