4ddfde68896bf07f56ce28db02f13fcc4280013b
Abner Castro <abnerfcastro@gmail.com>
2017-04-30 14:53:37 -0300

    trailing white spaces

--- 30-days-of-code/day-12/src/main.cpp
+++ 30-days-of-code/day-12/src/main.cpp
@@ -1,6 +1,6 @@
 /*!
  * src/main.cpp
- * 
+ *
  * main function for project
  * Author: Abner Castro
  * Date: April 9th, 2017
@@ -20,20 +20,21 @@ int main()
     string lastName;
     int id;
     int numScores;
-    
+
     cin >> firstName >> lastName >> id >> numScores;
-    
+
     vector<int> scores;
-  	for(int i = 0; i < numScores; i++) {
+    for (int i = 0; i < numScores; i++)
+    {
         int tmpScore;
         cin >> tmpScore;
-		scores.push_back(tmpScore);
-	}
+        scores.push_back(tmpScore);
+    }
 
-	Student* s = new Student(firstName, lastName, id, scores);
-	
+    Student *s = new Student(firstName, lastName, id, scores);
+
     s->printPerson();
-	cout << "Grade: " << s->calculate() << "\n";
-	
+    cout << "Grade: " << s->calculate() << "\n";
+
     return 0;
 }
